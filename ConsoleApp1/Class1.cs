ea76979ecf78e8f2501ea2df7fc562b6d2a6d8f0
宏 郭 <snellguo@outlook.com>
2023-08-13 11:18:50 +0800

    添加项目文件。

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConsoleApp2
+{
+    public class Class1s
+    {
+        delegate void MyDelegate();
+
+        public Class1s()
+        {
+            MyDelegate myDelegate = Show;
+            myDelegate += Show2;
+
+            Delegate[] arrDel = myDelegate.GetInvocationList();
+
+            foreach (var item in arrDel)
+            {
+                item.DynamicInvoke();
+            }
+
+        }
+        void Show()
+        {
+
+            try
+            {
+            Console.WriteLine("Show");
+                throw new Exception("error");
+
+            }
+            catch { 
+            Console.WriteLine("error");
+
+            }
+        }
+        void Show2()
+        {
+            Console.WriteLine("Show 2");
+            throw new Exception("error");
+
+        }
+        void Show(string str)
+        {
+
+        }
+        public void test()
+        {
+
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using ConsoleApp2;
+using System;
+using Xunit;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Class1s();
+            Console.ReadKey();
+        }
+    }
+
+}
